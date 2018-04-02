776d345cc6ab208e2916880a1b5e70bae80ca7ca
Kevin <kevin@rocksolidknowledge.com>
2018-04-02 15:10:01 +0100

    Tidied up injectiopn code to use the global injector

--- Expenses.UI/ClientApp/src/app/components/expenses/expenses-caims.service.spec.ts
+++ Expenses.UI/ClientApp/src/app/components/expenses/expenses-caims.service.spec.ts
@@ -11,6 +11,9 @@ import { StoreHelper } from '../../shared/store/store-helper';
 import { Store, InjectableStoreDecorator } from '../../shared/store/store';
 
 describe('ExpenseClaimsService', () => {
+  let service: ExpenseClaimsService;
+  let backend: HttpTestingController;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
@@ -30,92 +33,84 @@ describe('ExpenseClaimsService', () => {
       ],
       imports: [HttpClientTestingModule]
     });
+
+    service = TestBed.get(ExpenseClaimsService);
+    backend = TestBed.get(HttpTestingController);
   });
 
-  afterEach(
-    inject([HttpTestingController], (backend: HttpTestingController) => {
-      backend.verify();
+  afterEach(() => {
+    backend.verify();
+  });
+
+  // prettier-ignore
+  it('should be created', inject([ExpenseClaimsService], (service1: ExpenseClaimsService) => {
+      expect(service1).toBeTruthy();
     })
   );
 
   // prettier-ignore
-  it('should be created', inject([ExpenseClaimsService], (service: ExpenseClaimsService) => {
-      expect(service).toBeTruthy();
+  it('should call the http service to get the data',
+      inject([ExpenseClaimsService, HttpTestingController], (service1: ExpenseClaimsService, backend1: HttpTestingController) => {
+      service.claims().subscribe();
+      backend1.expectOne((req: HttpRequest<any>) => {
+        return req.url === '/api/expenses' && req.method === 'GET';
+      }, `GET Claims`);
     })
   );
 
   // prettier-ignore
-  it('should call the http service to get the data', async(
-      inject([ExpenseClaimsService, HttpTestingController], (service: ExpenseClaimsService, backend: HttpTestingController) => {
-        service.claims().subscribe();
-        backend.expectOne((req: HttpRequest<any>) => {
-          return req.url === '/api/expenses' && req.method === 'GET';
-        }, `GET Claims`);
+  it('should call the http service to get the data (sync)',    () => {
+      service.claims().subscribe();
+      backend.expectOne((req: HttpRequest<any>) => {
+        return req.url === '/api/expenses' && req.method === 'GET';
+      }, `GET Claims`);
+  });
+
+  it('should call the http service 1', () => {
+    service.claims().subscribe();
+
+    const req = backend.expectOne({
+      url: '/api/expenses',
+      method: 'GET'
+    });
+
+    expect(req.request.method).toBe('GET');
+  });
+
+  it('should call the http service 2', () => {
+    service.claims().subscribe();
+
+    const req = backend.expectOne({});
+
+    expect(req.request.method).toBe('GET');
+    expect(req.request.url).toBe('/api/expenses');
+  });
+
+  it('should return the correct data', () => {
+    const data = {
+      claims: [{ description: 'Desctiption1' }, { description: 'Desctiption2' }, { description: 'Desctiption3' }, { description: 'Desctiption4' }]
+    };
+
+    service.claims().subscribe((summary: ExpensesSummary) => {
+      expect(summary.claims.length).toBe(4);
+    });
+
+    backend
+      .expectOne({
+        url: '/api/expenses',
+        method: 'GET'
       })
-    )
-  );
+      .flush(data);
+  });
 
-  // prettier-ignore
-  it('should call the http service 1', async(
-      inject([ExpenseClaimsService, HttpTestingController], (expenseClaimsService: ExpenseClaimsService, backend: HttpTestingController) => {
-        expenseClaimsService.claims().subscribe();
-
-        const req = backend.expectOne({
-          url: '/api/expenses',
-          method: 'GET'
-        });
-
-        expect(req.request.method).toBe('GET');
-      })
-    )
-  );
-
-  // prettier-ignore
-  it('should call the http service 2', async(
-      inject([ExpenseClaimsService, HttpTestingController], (expenseClaimsService: ExpenseClaimsService, backend: HttpTestingController) => {
-        expenseClaimsService.claims().subscribe();
-
-        const req = backend.expectOne({});
-
-        expect(req.request.method).toBe('GET');
-        expect(req.request.url).toBe('/api/expenses');
-      })
-    )
-  );
-
-  // prettier-ignore
-  it('should return the correct data', async(
-      inject([ExpenseClaimsService, HttpTestingController], (expenseClaimsService: ExpenseClaimsService, backend: HttpTestingController) => {
-        const data = {
-          claims: [{ description: 'Desctiption1' }, { description: 'Desctiption2' }, { description: 'Desctiption3' }, { description: 'Desctiption4' }]
-        };
-
-        expenseClaimsService.claims().subscribe((summary: ExpensesSummary) => {
-          expect(summary.claims.length).toBe(4);
-        });
-
-        backend
-          .expectOne({
-            url: '/api/expenses',
-            method: 'GET'
-          })
-          .flush(data);
-      })
-    )
-  );
-
-  // prettier-ignore
-  it('should call the http service to create a new claim', async(
-      inject([ExpenseClaimsService, HttpTestingController], (service: ExpenseClaimsService, backend: HttpTestingController) => {
-        service.newClaim(new ExpenseClaim()).subscribe();
-        backend.expectOne(
-          {
-            url: '/api/expenses',
-            method: 'POST'
-          },
-          `Create Claim`
-        );
-      })
-    )
-  );
+  it('should call the http service to create a new claim', () => {
+    service.newClaim(new ExpenseClaim()).subscribe();
+    backend.expectOne(
+      {
+        url: '/api/expenses',
+        method: 'POST'
+      },
+      `Create Claim`
+    );
+  });
 });
